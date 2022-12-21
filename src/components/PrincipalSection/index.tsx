bc8c67050b9df1b588da72d97bdfc0705e9dda37
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2022-12-21 00:50:14 -0300

    📦 NEW: add the flex things

--- src/components/PrincipalSection/index.tsx
+++ src/components/PrincipalSection/index.tsx
@@ -12,26 +12,39 @@ const PrincipalSection = () => {
   return (
     <div className="min-h-[738px] bg-1 ">
       <Navbar />
-      <div>
-        <img src={Diablo4} alt="Diablo 4" />
-        <img src={HearthStone} alt="HearthStone" />
-        <img src={WoW} alt="World of Warcraft" />
-        <img src={Diablo1} alt="Diablo 1" />
-        <img src={StarCraft2} alt="Star Craft 2" />
-      </div>
-      <div className="font-poppins font-medium text-white">
-        <h1 className="text-6xl font-bold">Retorne à escuridão com o game Diablo IV</h1>
-        <p className="text-lg font-normal">
-          O retorno de Lilith traz uma era de escuridão e sofrimento
-        </p>
-        <button className="rounded	bg-primary px-8 py-3.5">Jogue Agora</button>
-      </div>
-      <div>
-        <img src={Diablo4XL} alt="Diablo 4" />
-
-        <div>
-          <p className="font-poppins text-lg font-semibold text-white">ASSISTA O TRAILER</p>
-          <img src={ThumbDiablo4} alt="Thumb Video Diablo 4" />
+      <div className="flex min-h-[738px] items-center justify-center">
+        <ul className="flex flex-col gap-y-5">
+          <li tabIndex={0}>
+            <img src={Diablo4} alt="Diablo 4" width="48" />
+          </li>
+          <li tabIndex={0}>
+            <img src={HearthStone} alt="HearthStone" width="48" />
+          </li>
+          <li tabIndex={0}>
+            <img src={WoW} alt="World of Warcraft" width="48" />
+          </li>
+          <li tabIndex={0}>
+            <img src={Diablo1} alt="Diablo 1" width="48" />
+          </li>
+          <li tabIndex={0}>
+            <img src={StarCraft2} alt="Star Craft 2" width="48" />
+          </li>
+        </ul>
+        <div className="ml-44 mr-64 w-full max-w-[562px] font-poppins font-medium	text-white">
+          <h1 className="mb-4 text-6xl font-bold leading-[71px]">
+            Retorne à escuridão com o game Diablo IV
+          </h1>
+          <p className="mb-8 text-lg font-normal">
+            O retorno de Lilith traz uma era de escuridão e sofrimento
+          </p>
+          <button className="rounded	bg-primary px-8 py-3.5">Jogue Agora</button>
+        </div>
+        <div className="flex flex-col content-between	">
+          <img src={Diablo4XL} alt="Diablo 4" className="mb-28" />
+          <div>
+            <p className="font-poppins text-lg font-semibold text-white">ASSISTA O TRAILER</p>
+            <img src={ThumbDiablo4} alt="Thumb Video Diablo 4" />
+          </div>
         </div>
       </div>
     </div>
